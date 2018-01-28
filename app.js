36f04bf317347b4f3cfcda6287408262926dbf47
Prabhu CS <sprabhuche@gmail.com>
2018-01-29 01:50:56 +0530

    app installing

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path=require('path');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+const passport=require('passport');
+const mongoose=require('mongoose');
+
+const app=express();
+
+const users=require('./routes/users')
+
+//Port Number
+const port=3000;
+
+//CORS Middleware
+app.use(cors());
+
+//Body Parser Middleware
+app.use(bodyParser.json()); 
+
+
+app.use('/users',users);
+
+
+// Index Route
+app.get('/',(req,res)=>{
+   res.send('Invalid Endpoint'); 
+});
+
+//Start Server
+app.listen(port,()=>{
+    console.log('Server started on port'+port);
+});
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const router=express.Router();
+
+
+//Register
+router.get('/register',(req,res,next)=>{
+   res.send('Register'); 
+});
